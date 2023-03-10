d4169110a72eebfa816a8781ba79a1701ccf0df9
Jetro Kepler <jetrokeplergomes@gmail.com>
2023-03-10 20:03:17 -0300

    Projeto.

---
+++ miniprojeto/main.py
@@ -0,0 +1,14 @@
+# Lógica de programação.
+
+# 1 - Importar a base de dados;
+# 2 - Visuallizar a base de dados;
+# 3 - Calcular o faturamento por loja;
+# 4 - Calcular a quantidade de produtos vendidos por loja;
+# 5 - Calcular o ticket médio por loja;
+# 6 - Enviar um e-mail com o relatório.
+
+# Instalação e importação das bibliotecas.
+
+import pandas as pd
+
+# Importando a base de dados.
