f0b0c6d8a2c0bbf1e09b4ede10e9dbe5276b16c6
Jetro Kepler <jetrokeplergomes@gmail.com>
2023-03-17 19:25:37 -0300

    Projetos.

--- Projetos/projeto 3/main.py
+++ Projetos/projeto 3/main.py
@@ -5,15 +5,40 @@
 # 5 - Como cada característica do cliente impacata na nota.
 
 import pandas as pd
+import plotly.express as px # Para fazer gráficos.
 
-tabela = pd.read_csv("Projetos\projeto 3\clientes.csv", encoding="latin", sep=";") # Usa-se o "encoding='latin'" para não dar erro por conta de caracteres especiais.
+tabela = pd.read_csv("projeto 3\clientes.csv", encoding="latin", sep=";") # Usa-se o "encoding='latin'" para não dar erro por conta de caracteres especiais.
+
+pd.set_option("display.max_columns", None)
+
+# print(tabela.info()) Serve para ver as informações das colunas.
 
 tabela = tabela.drop("Unnamed: 8", axis=1) # Deletando a coluna "Unnamed: 8" pois não há nada nela. Axis = 0 se for linha e axis = 1 se for coluna.
 
 tabela["Salário Anual (R$)"] = pd.to_numeric(tabela["Salário Anual (R$)"], errors="coerce") # Corrigindo a coluna "Salário Anual (R$)" para transformá-la em número, e forçando o que não é número a tornar número.
 
+# print(tabela[["Profissão"]].isna()) para ver as linhas vazias.
+
+tabela = tabela.dropna() # Jogando fora informações vazias, pois não há como analisar um cliente se sua profissão estiver vazia.
+
 print(tabela)
 
-print(tabela.info()) # Serve para ver as informações das colunas.
+print("="*70)
 
-# Corrigir informações vazias (imaginando que um cliente não tem sua profissão e sua nota na tabela. Não tem como considerar ele, então tem que corrigir).
+print(tabela.describe()) # Entendendo as notas dos clientes.
+
+# O "mean" significa média, e é o balizador, por exemplo, os clientes com nota maior que 53 são os que se procura.
+
+for coluna in tabela.columns:
+
+    grafico = px.histogram(tabela, x = coluna, y = "Nota (1-100)", histfunc = "avg", text_auto = True) # Criando o gráfico, colocando os números no gráfico e fazendo ele fazer uma média das notas.
+
+    grafico.show()
+    
+# Perfil ideal de cliente:
+
+# Idade > 15 anos (não tem muita diferença entre as faixas etárias depois disso);
+# Faixa salarial não parece fazer muita diferença;
+# Áreas de trabalho: Entretenimento e artista (Evitar construção);
+# tem entre 10 e 15 anos de experiência;
+# Com famílias não tão grandes (até no máx. 7 pessoas)
--- Projetos/projeto 4/main.py
+++ Projetos/projeto 4/main.py
@@ -7,13 +7,13 @@
 import pandas as pd
 from selenium import webdriver # A vantagem de importar a penas o webdriver e não todo o selenium é fazer o código ficar mais leve. O selenium, diferente do pyautogui, faz o código rodar em sugundo plano, e outra vantagem é que ele sabe a hora de funcionar, ou seja, ele reconhece a hora que uma aba está abrindo, por exemplo.
 
-tabela = pd.read_excel("Projetos\projeto 4\commodities.xlsx")
+tabela = pd.read_excel("projeto 4\commodities.xlsx")
 
 navegador = webdriver.Chrome() # Abrindo o navegador.
 
 for linha in tabela.index: # O python só reconhece "linha" por causa que "tabela.index" é uma lista que tem todas as linhas da tabela. O "for" está sendo utilizado para que, em cada linha da coluna "Produto", seja encontrado o preço atual do produto no site informado. 
 
-    produto = tabela.loc(linha, "Produto")
+    produto = tabela.loc[linha, "Produto"] # Localizando uma célula da tabela de acordo com sua linha e sua coluna
 
     print(produto)
 
@@ -23,12 +23,20 @@ for linha in tabela.index: # O python só reconhece "linha" por causa que "tabel
 
     navegador.get(link) # Seleciona o site.
 
-    preco = navegador.find_element('xpath', '//*[@id="comercial"]').get_attribute("value") # Procurando um elemento na página.
+    preco = navegador.find_element('xpath', '//*[@id="comercial"]').get_attribute("value") # Procurando um elemento na página e pegando um valor, no caso, o do preço.
 
-    tabela.loc[linha, "Preço Atual"] = preco # Fazendo o Preço atual receber os valores.
+    # .click() serve para clicar;
+    # .send_keys("texto") para escrever;
+    # .get_atribute() para pegar um valor.
+
+    preco = preco.replace(".", "").replace(",", ".") # Colocando no formato americano.
+
+    tabela.loc[linha, "Preço Atual"] = float(preco) # Fazendo a coluna "Preço Atual" da tabela receber os valores. O "float" foi usado para que a tablea receba um número e não uma string.
 
     print(preco)
 
-# .click() serve para clicar;
-# .send_keys("texto") para escrever;
-# .get_atribute() para pegar um valor.
+tabela["Comprar"] = tabela["Preço Atual"] < tabela["Preço Ideal"]
+
+print(tabela)
+
+tabela.to_excel("projeto 4\commodities_atualizado.xlsx", index = False) # Criando a tabela atualizada e excluindo a coluna de índice.
